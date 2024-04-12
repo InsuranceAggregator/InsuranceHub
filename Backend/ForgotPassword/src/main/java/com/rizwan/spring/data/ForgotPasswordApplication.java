917beddb90938ed0d062f9b16abcb5a19274efd2
Rizwan <rijjuclash@gmail.com>
2024-04-12 16:38:54 +0530

    Final ForgotPassword model

---
+++ Backend/ForgotPassword/src/main/java/com/rizwan/spring/data/ForgotPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.rizwan.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgotPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgotPasswordApplication.class, args);
+	}
+
+}
---
+++ Backend/ForgotPassword/src/main/java/com/rizwan/spring/data/entity/ResetPassword.java
@@ -0,0 +1,83 @@
+package com.rizwan.spring.data.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="userdetails")
+public class ResetPassword {
+
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private int id;
+	    private String name;
+	    private String mobilenumber;
+	    private String email;
+	    private String password;
+	    private String confirmpassword;
+	    private String address;
+	    private String description;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getMobilenumber() {
+			return mobilenumber;
+		}
+		public void setMobilenumber(String mobilenumber) {
+			this.mobilenumber = mobilenumber;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getConfirmpassword() {
+			return confirmpassword;
+		}
+		public void setConfirmpassword(String confirmpassword) {
+			this.confirmpassword = confirmpassword;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		@Override
+		public String toString() {
+			return "ResetPassword [id=" + id + ", name=" + name + ", mobilenumber=" + mobilenumber + ", email=" + email
+					+ ", password=" + password + ", confirmpassword=" + confirmpassword + ", address=" + address
+					+ ", description=" + description + "]";
+		}
+	    
+		
+		}
+	    
+
+
+
