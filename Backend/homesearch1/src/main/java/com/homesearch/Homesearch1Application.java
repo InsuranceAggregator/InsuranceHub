20b636180c6145ac4da5327ff2532218853af97c
Rizwan <rijjuclash@gmail.com>
2024-04-12 17:10:35 +0530

    Final homesearch1 model

---
+++ Backend/homesearch1/src/main/java/com/homesearch/Homesearch1Application.java
@@ -0,0 +1,13 @@
+package com.homesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homesearch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homesearch1Application.class, args);
+	}
+
+}
