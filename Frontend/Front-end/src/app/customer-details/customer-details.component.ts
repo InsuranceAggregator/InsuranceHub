3a38c8d0f730d4a8b7fe029aac656cb78929ee2b
Rizwan <rijjuclash@gmail.com>
2024-04-17 15:54:50 +0530

    Sent form-data from customerDetailComponent to paymentGateway component

--- Frontend/Front-end/src/app/customer-details/customer-details.component.ts
+++ Frontend/Front-end/src/app/customer-details/customer-details.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
+import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { UserDetailsService } from '../user-details.service';
  
@@ -10,25 +10,26 @@ import { UserDetailsService } from '../user-details.service';
   styleUrls: ['./customer-details.component.css']
 })
 export class CustomerDetailsComponent {
+      // Initialize variables for start and end dates
+  startDate: string | null = null;
+  endDate: string | null = null;
   submitted = false;
-  constructor(private userDetailsService: UserDetailsService,private router: Router) { }
+  constructor(private userDetailsService: UserDetailsService,private router: Router,private datePipe: DatePipe) {
+    const currentDate = new Date();
+        this.startDate = this.datePipe.transform(currentDate, 'yyyy-MM-dd');
+        const nextYearDate = new Date(currentDate.getFullYear() + 1, currentDate.getMonth(), currentDate.getDate());
+        this.endDate = this.datePipe.transform(nextYearDate, 'yyyy-MM-dd');
+   }
  
   onSubmit(form: NgForm) {
     this.submitted = true;
     if (form.valid) {
      
-      this.userDetailsService.submitUserDetails(form.value).subscribe(
-        response => {
-         
-          console.log('Form data submitted successfully:', response);
-          this.router.navigate(['/paymentGateway']);
-        },
-        error => {
-         
-          console.error('Error submitting form data:', error);
-         
-        }
-      );
+       // 1. Capture the form data
+       const formData = form.value;
+
+       // 2. Navigate to the payment gateway page
+       this.router.navigate(['/paymentGateway'], { state: { formData } });
     } else {
       console.log('Form is invalid.');
       alert("Check the errors")
