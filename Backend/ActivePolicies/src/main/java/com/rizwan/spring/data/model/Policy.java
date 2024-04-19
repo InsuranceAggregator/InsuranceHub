11c239e3647fecbda2be5e9bdc397d151520cdc2
Rizwan <rijjuclash@gmail.com>
2024-04-19 10:10:06 +0530

    Added start & End-date fields

--- Backend/ActivePolicies/src/main/java/com/rizwan/spring/data/model/Policy.java
+++ Backend/ActivePolicies/src/main/java/com/rizwan/spring/data/model/Policy.java
@@ -1,5 +1,7 @@
 package com.rizwan.spring.data.model;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,100 +24,97 @@ public class Policy {
     private String policyNumber;
     private String policyType;
     private double premium;
-    
+    private LocalDate startDate;
+    private LocalDate endDate;
     // Constructors
     public Policy() {
     }
-    
-    public Policy(String name, String email, String mobileNumber, int age, String gender, String policyNumber, String policyType, double premium) {
-        this.name = name;
-        this.email = email;
-        this.mobileNumber = mobileNumber;
-        this.age = age;
-        this.gender = gender;
-        this.policyNumber = policyNumber;
-        this.policyType = policyType;
-        this.premium = premium;
-    }
-    
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getPolicyNumber() {
-        return policyNumber;
-    }
-
-    public void setPolicyNumber(String policyNumber) {
-        this.policyNumber = policyNumber;
-    }
-
-    public String getPolicyType() {
-        return policyType;
-    }
-
-    public void setPolicyType(String policyType) {
-        this.policyType = policyType;
-    }
-
-    public double getPremium() {
-        return premium;
-    }
-
-    public void setPremium(double premium) {
-        this.premium = premium;
-    }
-    
-    // toString method
-    @Override
-    public String toString() {
-        return "InsurancePolicy [id=" + id + ", name=" + name + ", email=" + email + ", mobileNumber=" + mobileNumber
-                + ", age=" + age + ", gender=" + gender + ", policyNumber=" + policyNumber + ", policyType="
-                + policyType + ", premium=" + premium + "]";
-    }
+	public Policy(Long id, String name, String email, String mobileNumber, int age, String gender, String policyNumber,
+			String policyType, double premium, LocalDate startDate, LocalDate endDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.mobileNumber = mobileNumber;
+		this.age = age;
+		this.gender = gender;
+		this.policyNumber = policyNumber;
+		this.policyType = policyType;
+		this.premium = premium;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPolicyNumber() {
+		return policyNumber;
+	}
+	public void setPolicyNumber(String policyNumber) {
+		this.policyNumber = policyNumber;
+	}
+	public String getPolicyType() {
+		return policyType;
+	}
+	public void setPolicyType(String policyType) {
+		this.policyType = policyType;
+	}
+	public double getPremium() {
+		return premium;
+	}
+	public void setPremium(double premium) {
+		this.premium = premium;
+	}
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+	@Override
+	public String toString() {
+		return "Policy [id=" + id + ", name=" + name + ", email=" + email + ", mobileNumber=" + mobileNumber + ", age="
+				+ age + ", gender=" + gender + ", policyNumber=" + policyNumber + ", policyType=" + policyType
+				+ ", premium=" + premium + ", startDate=" + startDate + ", endDate=" + endDate + "]";
+	}
 }
+   
