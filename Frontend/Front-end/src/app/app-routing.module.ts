25ef573138529dde06892c8441ef9f132073dbf9
Rizwan <rijjuclash@gmail.com>
2024-04-19 20:40:34 +0530

    Added new path for claim-form component

--- Frontend/Front-end/src/app/app-routing.module.ts
+++ Frontend/Front-end/src/app/app-routing.module.ts
@@ -20,6 +20,7 @@ import { CustomerDetailsComponent } from './customer-details/customer-details.co
 import { CustomerSupportComponent } from './customer-support/customer-support.component';
 import { PaymentGatewayComponent } from './payment-gateway/payment-gateway.component';
 import { ActivePoliciesComponent } from './active-policies/active-policies.component';
+import { ClaimFormComponent } from './claim-form/claim-form.component';
 
 
 
@@ -46,7 +47,8 @@ const routes: Routes = [
   { path: 'customer-details', component: CustomerDetailsComponent},
   { path: 'customer-support', component: CustomerSupportComponent },
   { path: 'paymentGateway', component: PaymentGatewayComponent},
-  { path: 'activepolicies', component: ActivePoliciesComponent}
+  { path: 'activepolicies', component: ActivePoliciesComponent},
+  { path: 'claim-form', component: ClaimFormComponent}
 ];
 
 @NgModule({
